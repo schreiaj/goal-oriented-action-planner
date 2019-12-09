065b84e2926c41284f463446bc504aad1ee89cbd
Andrew Schreiber <andrew.schreiber@owllabs.com>
2019-12-08 21:53:01 -0700

    Added license

--- lib/condition.d.ts
+++ lib/condition.d.ts
@@ -1,7 +1,11 @@
-import { World } from './world';
+import { World } from "./world";
+import * as jsonata from "jsonata";
 export declare class Condition {
     name: string;
-    value: boolean;
-    constructor(name: string, value: boolean);
+    value: boolean | jsonata.Expression;
+    private query?;
+    private isStatic;
+    constructor(name: string, value: boolean | string);
     isMet: (world: World) => boolean;
+    output: (world: World) => any;
 }
--- lib/world.d.ts
+++ lib/world.d.ts
@@ -1,6 +1,6 @@
 import { Condition } from "./condition";
 export interface World {
-    [key: string]: boolean;
+    [key: string]: any;
 }
 export declare const updateWorld: (world: World, updates: Condition[]) => World;
 export declare const isGoalMet: (world: World, goal: Condition[]) => boolean;
